54d85bf799d691f41b89e9a4e708753f26d80310
Андрей Ковлев (SeReRaGuy) <sereraguy654@gmail.com>
2024-04-19 18:41:36 +0300

    Сделана лабораторная работа №3

---
+++ Lab3M/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const Mas1 = [123, 456, "Numbers"]; //Переменная объявленного типа
+console.log(Mas1); //Вывод переменной
+const Mas2 = ["Sun", null, "RandomText", 3];
+console.log(Mas2);
+//Для компиляции TypeScript - tsc
+//Для запуска JavaScrypt = node <ИМЯ_ФАЙЛА>.js
